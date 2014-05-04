db621a4313aca77fcca0791baa6acd00b1591b0b
Egor Firsov <melnik88@msn.com>
2014-05-04 12:57:27 +0400

    add login form

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,21 +5,40 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-    .controller('loginCtrl',['$scope', function(){
-    }])
-    .controller('appCtrl',['$scope', function(){
-    }])
-    .controller('pointsCtrl', ['$scope', 'geolocation','MAP_PARAMS','$firebase', function ($scope, geolocation, MAP_PARAMS, $firebase) {
-        var getMap,
-            firebaseConnect;
+    .controller('loginCtrl', function ($scope, $rootScope, $location) {
+        $scope.character = 'zombie';
+
+        $scope.loginUser = function (name, character) {
+            switch (character) {
+            case "zombie_boy":
+                $rootScope.character = {
+                    name: name,
+                    xp: 20,
+                    coords:
+                        {
+                            lat: 0,
+                            lon: 0
+                        }
+                };
+                break;
+            }
+            $location.path('/app');
+
+
+        };
+
+
+    })
+    .controller('appCtrl', function ($scope, geolocation, $rootScope, MAP_PARAMS, $firebase) {
+        var pushMyDataToFirebase,
+            firebaseConnect,
+            createToken;
+        console.log($rootScope.character);
+
 
         firebaseConnect = new Firebase("https://mymaps.firebaseio.com");
-        firebaseConnect.on('child_added', function(snapshot) {
-            $scope.CoordsData = snapshot.val();
-
-        });
-
 //      масштаб карты (метры, брать с карты)
+
         $scope.mapScale = MAP_PARAMS.SCALE;
         $scope.CanvasWidth = 658;
         $scope.CanvasHeight = 856;
@@ -36,22 +55,38 @@ angular.module('myApp.controllers', [])
         $scope.x_coef = Math.abs(MAP_PARAMS.WIDTH / ($scope.lon_1 - $scope.lon_0));
 
 
-        getMap = function () {
-            geolocation.getLocation().then( function (data) {
+        firebaseConnect.on('child_added', function(snapshot) {
+            $scope.CoordsData = snapshot.val();
+
+        });
+
+
+        createToken = function () {
+            return Math.random().toString(36).substr(2);
+        };
+        $rootScope.token = createToken();
+
+        pushMyDataToFirebase = function () {
+            var obj = {};
+            console.log($rootScope.character.name);
+            geolocation.getLocation().then(function (data) {
                 $scope.coords = { lat: data.coords.latitude, long: data.coords.longitude };
-                firebaseConnect.push({name: "vasia", coords: $scope.coords});
+                obj[$rootScope.token] = {
+                    name: $rootScope.character.name,
+                    coords: $scope.coords,
+                    xp: $rootScope.character.xp
+                };
+                firebaseConnect.update(obj);
 
             });
         };
-
-        getMap();
-        console.log($scope.CoordsData);
+        pushMyDataToFirebase();
 
 
 
 
-//        setInterval(getMyLocation, '1000');
-    }]);
+        setInterval(pushMyDataToFirebase, '1000');
+    });
 //  .controller('MyCtrl2', [function() {
 //
 //  }]);
