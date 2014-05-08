d978fead5ebab2ba064bd4417520c85d905f17df
Egor Firsov <efirsov@vedomosti.ru>
2014-05-08 13:49:26 +0400

    some code clear

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -32,15 +32,7 @@ angular.module('myApp.controllers', [])
 
             $location.path('/app');
 //            тут можно создавать и присваивать уникальный токен
-
-
-
-
-
-
         };
-
-
     })
     .controller('appCtrl', function ($scope, geolocation, $rootScope, MAP_PARAMS, $firebase, localStorageService) {
         var pushMyDataToFirebase,
@@ -53,7 +45,8 @@ angular.module('myApp.controllers', [])
         $scope.mapScale = MAP_PARAMS.SCALE;
         $scope.CanvasWidth = 658;
         $scope.CanvasHeight = 856;
-//  салтыковка
+
+//      салтыковка
 ////        левый верхний угол
 //        $scope.lat_0 = 55.745992;
 //        $scope.lon_0 = 37.925026;
@@ -61,49 +54,44 @@ angular.module('myApp.controllers', [])
 //        $scope.lat_1 = 55.742404;
 //        $scope.lon_1 = 37.929984;
 
-//        железнодорожный
-
-//        левый верхний угол
+//      железнодорожный
+//      левый верхний угол
         $scope.lat_0 = 55.753200;
         $scope.lon_0 = 38.004990;
-//        правый нижний угол
+//      правый нижний угол
         $scope.lat_1 = 55.746016;
         $scope.lon_1 = 38.015001;
 
-
         $scope.dotScale = 6;
-
 
         $scope.y_coef = Math.abs(MAP_PARAMS.HEIGHT / ($scope.lat_1 - $scope.lat_0));
         $scope.x_coef = Math.abs(MAP_PARAMS.WIDTH / ($scope.lon_1 - $scope.lon_0));
 
-        playerInit = function (){
-                $rootScope.token = localStorageService.get('token');
-                var dataRef = new Firebase('https://mymaps.firebaseio.com/' + $rootScope.token);
-                dataRef.on('value', function(snapshot) {
-                    $rootScope.character = snapshot.val();
-                    console.log($rootScope.character)
-                });
+        $scope.pushInterval = null;
+
+        playerInit = function () {
+            $rootScope.token = localStorageService.get('token');
+            var dataRef = new Firebase('https://mymaps.firebaseio.com/' + $rootScope.token);
+            dataRef.on('value', function(snapshot) {
+                $rootScope.character = snapshot.val();
+            });
             console.log('player is inited')
         }
 
         getAllData = function () {
             firebaseConnect.on('child_added', function (snapshot) {
-//            console.log(snapshot.val().coords);
             console.log('data is updated');
             $scope.CoordsData = snapshot.val().coords;
-
         });
-
         }
 
         if ($rootScope.token == undefined) { playerInit(); }
 
+        console.log($rootScope.token);
 
         pushMyDataToFirebase = function () {
             geolocation.getLocation().then(function (data) {
                 var obj = {};
-
                 $scope.coords = {lat: data.coords.latitude, lon: data.coords.longitude };
                 obj[$rootScope.token] = {
                     name: $rootScope.character.name,
@@ -111,11 +99,17 @@ angular.module('myApp.controllers', [])
                     xp: $rootScope.character.xp
                 };
                 firebaseConnect.update(obj, getAllData);
-
             });
-        };
+        }
 
-        setInterval(pushMyDataToFirebase, '2000');
+        $rootScope.$watch('token', function() {
+            window.clearInterval($scope.pushInterval);
+            $scope.pushInterval = setInterval(pushMyDataToFirebase, 2000)
+        });
+
+        if ($rootScope.token == undefined) playerInit();
+
+
 
     });
 
